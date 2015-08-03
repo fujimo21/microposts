ba8a06ec1f2afebd8088aaa30a9970aa47e39a00
fujimo21 <y.fujomoto@gmail.com>
2015-08-03 14:02:02 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -16,11 +17,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:update] = "Updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
